afca6898cbfc186b6a1d29584fa0e92dafcbc39f
Miezhiko <Miezhiko@gmail.com>
2025-10-04 10:17:00 +0400

    increase news delay
    Signed-off-by: Miezhiko <Miezhiko@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -92,7 +92,7 @@ async fn main() -> anyhow::Result<()> {
     Id::new(options::CONFIG.twitter_channel_id)
   );
 
-  let _rss_handle = rss_subscriber.start(Duration::from_secs(3000));
+  let _rss_handle = rss_subscriber.start(Duration::from_secs(6000));
   tracing::info!("Twitter RSS subscriber has started");
 
   while let Some(item) = shard.next_event(EventTypeFlags::all()).await {
